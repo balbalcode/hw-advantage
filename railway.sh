38a426b1e1f8333d61bfa9bb5efec7b8ae41ffb1
balbalcode <iqbalmaulana027@gmail.com>
2025-12-05 01:28:20 +0700

    fix: remove unused user

--- railway.sh
+++ railway.sh
@@ -13,8 +13,5 @@ echo "Caching config..."
 php artisan config:cache
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
-
 echo "Starting server..."
 php artisan serve --host=0.0.0.0 --port=${PORT:-8000}
